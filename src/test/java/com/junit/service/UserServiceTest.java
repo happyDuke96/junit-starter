89d1719291116158dab00dab3179b9198904064b
AbbosAbduhakimov <abuabdulloh9113@gmail.com>
2023-12-09 17:56:06 +0500

    Setup junit-starter

---
+++ src/test/java/com/junit/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    public void test(){
+        assertTrue(false);
+    }
+}
