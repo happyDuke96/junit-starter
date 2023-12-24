b0de45d735155d18d47a851d74065ab1df567c93
AbbosAbduhakimov <abuabdulloh9113@gmail.com>
2023-12-24 14:27:02 +0500

    MockitoExtension and BDDMockito

--- src/test/java/com/junit/service/extension_model/mockito_sample/UserServiceMockitoSpyTest.java
+++ src/test/java/com/junit/service/extension_model/mockito_sample/UserServiceMockitoSpyTest.java
@@ -9,13 +9,15 @@ import com.junit.extension.UserServiceParameterResolver;
 import com.junit.service.UserService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-/** Mockito - фреймворк помогает создать фейковый(mock) объекты допустим хотим тестировать UserService,
+/**
+ * Mockito - фреймворк помогает создать фейковый(mock) объекты допустим хотим тестировать UserService,
  * для этого нужен UserRepository но нам не интересно как работает UserRepository в этом случае можем токовый объект с помощью Mockito
  * Test Doubles(дублёры) - это специальные определение которые пришел на замен настоящего объектa
  * Dummy - который не используется во время тестирование,нужны для заполнения параметров метода
@@ -24,18 +26,22 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Stub - объекты использует mocks и spies для ответа(Answer) на вызова метода во время теста
  * Mock - запрограммированный объект возвращаемый ожидаемый результат(stubs) на вызова определенного метода
  * Spy - proxy для реальных объектов который ведет себя точно также как настоящий,или запрограммированный как mock
- * */
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith({
         GlobalExtension.class,
         UserServiceParameterResolver.class,
         PostProcessingExtension.class,
         ConditionalExtension.class,
+        MockitoExtension.class
 })
 class UserServiceMockitoSpyTest {
 
-    private UserService userService;
-    private UserDao userDao;
+    @InjectMocks
+    private UserService userService; // объект вызывающий mock объект,доступно @ExtendWith -> MockitoExtension.class
+//    @Mock(strictness = Mock.Strictness.LENIENT)
+    @Spy
+    private UserDao userDao; // Mockito.mock() alternative,доступно @ExtendWith -> MockitoExtension.class
     private static final User JOHN = User.of(1, "John", "123");
     private static final User SARAH = User.of(2, "Sarah", "111");
 
@@ -48,8 +54,23 @@ class UserServiceMockitoSpyTest {
     @BeforeEach
     void prepare() {
         System.out.println("Before Each " + this);
-        this.userDao = Mockito.spy(new UserDao());
-        this.userService = new UserService(userDao);
+//        this.userDao = Mockito.spy(new UserDao());
+//        Mockito.lenient().when(userDao.delete(JOHN.getId())).thenReturn(true);
+        Mockito.doReturn(true).when(userDao).delete(JOHN.getId());
+//        Mockito.doThrow(RuntimeException.class).when(userDao).delete(JOHN.getId());
+
+//        this.userService = new UserService(userDao);
+    }
+
+    /*MockitoExtension по умолчанию инициализирует Stub в @BeforeEach фазе,
+      у него будет доступ к нему благодаря интерфейсов и ругает если будет тесты не использует эти stub
+      ниже метод не использует объявленный stub в @BeforeEach,но его поведение можно изменить,
+       решение если юзаем @Spy что внутри теста инициализировать,Если Mock сделать линентом */
+    @Test
+    void throwIfDatabaseNotAvailable() {
+        Mockito.doThrow(RuntimeException.class).when(userDao).delete(JOHN.getId());
+
+        assertThrows(RuntimeException.class,() -> userService.delete(JOHN.getId()));
     }
 
     @Test
@@ -63,7 +84,7 @@ class UserServiceMockitoSpyTest {
 //                .thenReturn(false);
 
         // вызывается внутри when(),т.е создается обертка потом вызывается метод его
-        Mockito.doReturn(true).when(userDao).delete(JOHN.getId());
+//        Mockito.doReturn(true).when(userDao).delete(JOHN.getId());
 
         var deleteResult = userService.delete(JOHN.getId());
         System.out.println(userService.delete(JOHN.getId()));
@@ -80,7 +101,7 @@ class UserServiceMockitoSpyTest {
 
         // можем отлавливать переданный значение передав какой тип
         var argumentCaptor = ArgumentCaptor.forClass(Integer.class);
-        Mockito.verify(userDao,Mockito.times(3)).delete(argumentCaptor.capture());
+        Mockito.verify(userDao, Mockito.times(3)).delete(argumentCaptor.capture());
 
         assertThat(argumentCaptor.getValue()).isEqualTo(25);
 
